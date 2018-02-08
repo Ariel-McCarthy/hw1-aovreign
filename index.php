a548efdd70f85e359311c085c263d4feb40e13fe
sovreign <somccarthy@csumb.edu>
2018-02-08 07:52:32 +0000

    2nd try

---
+++ index.php
@@ -0,0 +1,63 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset =  "utf-8"/>
+        <title>Programming Languages</title>
+        
+        <link href = "CSS/styles.css" rel = "stylesheet" type = "text/css" />
+        <link href="https://fonts.googleapis.com/css?family=Tangerine" rel="stylesheet"> 
+    </head>
+    <body>
+        <header>
+            <h1>CS Programming Languages</h1>
+        </header>
+        <nav>
+            <hr width = "50%"/>
+            <a href = "index.php" id = infoPgLinkColor>Home</a>
+            <a href = "c++.html">C++</a>
+            <a href = "python.html">Python</a>
+            <a href = "html.html">HTML</a>
+        </nav>
+
+        <main>
+            <figure id = "languagesPic">
+                <img src = "https://sdtimes.com/wp-content/uploads/2015/08/prog-languages.png">
+                <nav>
+                    <hr width = "20%"/>
+                    <a href = "https://sdtimes.com/wp-content/uploads/2015/08/prog-languages.png">Link to image source</a>
+                </nav>
+            </figure>
+            
+            <div id = "Page Info">
+                <br/>
+                An extremely important aspect of computer science is programming! <br/>
+                <p>
+                    Before someone begins to program, they must first decide which programming language to choose.<br/>
+                    Here you can find information to help you choose the right language for you,<br/> 
+                    depending on which type of outcome you'd like.<br/><br/>
+                    For example: Will you want to create a project?<br/> 
+                    Maybe you want graphics, maybe you want to program a robot, or you might want to follow weather patterns.<br/><br/>
+                    Simply navigate through the links in order to find the best language(s) for the work you want to produce.
+                </p>
+                <br/><br/>
+                <em>
+                    "The cheapest, fastest, and most reliable components are those that aren’t there."<br/>
+                    — Gordon Bell
+                </em>
+                <nav>
+                    <hr width = "20%"/>
+                    <a href = "http://quotes.cat-v.org/programming/">Link to quote source</a>
+                </nav>
+            </div>
+        </main>
+        <footer>
+            <hr>
+            CST 336-02_SP18 <br/>
+            &copy; McCarthy <br/>
+            
+            <figure id = "csumb_logo">
+                <img height = 100px width = 100px src = "https://csumb.edu/sites/default/files/styles/profile_large/public/photo_1_6.jpg?itok=N4I4g9Wl">
+            </figure>
+        </footer>
+    </body>
+</html>
